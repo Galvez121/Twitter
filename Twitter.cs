8a06ed5fd50bb49d385ba97662beec7f71ac4daf
Juan Galvez <juanjosegalvez2014@gmail.com>
2024-09-18 15:50:07 -0400

    First commit

---
+++ Twitter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Twitter
+{
+    public static void Main(string[] args)
+    {
+        // Module 3 Exercise 3.1
+
+        Console.WriteLine("Type your message");
+
+        string message = Console.ReadLine();
+
+        if(message.Length <= 140){
+            Console.WriteLine("Posted");
+        }
+        else{
+            Console.WriteLine("Rejected");
+        }
+      
+
+    }
+}
